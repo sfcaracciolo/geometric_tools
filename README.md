4a9903dcd2b78503425ddb73213915173582f4a4
scaracciolo <santiago.caracciolo@gmail.com>
2023-07-17 12:37:52 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# geometric tools
+
+Some basic geometric operations based on Open3D like:
+
+* project_to_sphere: project a point cloud to a sphere based on center and radius parameters.
+* project_to_surface: project a point cloud to a surface. The implementation use RaycastingScene (open3d).
+* compute_inner_sphere: compute parameters of a sphere which is inner of a point cloud given.
+* ...
---
+++ src/geometric_tools/core.py
@@ -0,0 +1,45 @@
+from typing import Tuple, Union, List
+import numpy as np 
+from open3d.utility import Vector3dVector, Vector3iVector
+from open3d.geometry import PointCloud, TriangleMesh
+from open3d.core import Tensor, float32, uint32
+from open3d.t.geometry import RaycastingScene
+
+def scene_surface(vertices: Vector3dVector, triangles: Vector3iVector) -> RaycastingScene:
+    t_vertices = Tensor(np.asarray(vertices), float32)
+    t_triangles = Tensor(np.asarray(triangles), uint32)
+    scene = RaycastingScene()
+    _ = scene.add_triangles(t_vertices, t_triangles)
+    return scene
+
+def project_to_surface(vertices: np.ndarray, surface: RaycastingScene) -> np.ndarray:
+    t_vertices = Tensor(vertices, float32)
+    return surface.compute_closest_points(t_vertices)['points'].numpy()
+
+def triangle_mesh_by_convex_hull_of_inner_sphere(vertices:Union[np.ndarray, Vector3dVector]) -> np.ndarray:
+    center, radius = compute_inner_sphere(vertices)
+    projected_vertices = project_to_sphere(vertices, center, radius/2.)
+    triangle_mesh, indices = compute_convex_hull(projected_vertices)
+    return triangle_mesh, indices
+
+def compute_convex_hull(vertices:Union[np.ndarray, Vector3dVector]) -> Tuple[Union[TriangleMesh, List[int]]]:
+    if isinstance(vertices, np.ndarray):
+        vertices = Vector3dVector(vertices)
+    pcd = PointCloud(vertices)
+    return pcd.compute_convex_hull()
+
+def compute_inner_sphere(vertices:Union[np.ndarray, Vector3dVector]) -> Tuple[Union[np.ndarray, float]]:
+    if isinstance(vertices, np.ndarray):
+        vertices = Vector3dVector(vertices)
+    pcd = PointCloud(vertices)
+    center = pcd.get_center()[np.newaxis, :]
+    distances_to_center = pcd.compute_point_cloud_distance(PointCloud(Vector3dVector(center)))
+    max_radius = np.asarray(distances_to_center).min()
+    return center, max_radius
+
+def project_to_sphere(vertices:Union[np.ndarray, Vector3dVector], center: np.ndarray, radius: float) -> np.ndarray:
+    if isinstance(vertices, Vector3dVector):
+        vertices = np.asarray(vertices)
+    m = vertices - center
+    m /= np.linalg.norm(m, axis=1, keepdims=True)
+    return center + radius * m
