3745eb26d68ffbf61f7ba37696c9e856671bbd4f
scaracciolo <santiago.caracciolo@gmail.com>
2023-07-21 12:51:28 -0300

    add coords convertions

--- src/geometric_tools/core.py
+++ src/geometric_tools/core.py
@@ -23,14 +23,12 @@ def triangle_mesh_by_convex_hull_of_inner_sphere(vertices:Union[np.ndarray, Vect
     return triangle_mesh, indices
 
 def compute_convex_hull(vertices:Union[np.ndarray, Vector3dVector]) -> Tuple[Union[TriangleMesh, List[int]]]:
-    if isinstance(vertices, np.ndarray):
-        vertices = Vector3dVector(vertices)
+    if isinstance(vertices, np.ndarray): vertices = Vector3dVector(vertices)
     pcd = PointCloud(vertices)
     return pcd.compute_convex_hull()
 
 def compute_inner_sphere(vertices:Union[np.ndarray, Vector3dVector]) -> Tuple[Union[np.ndarray, float]]:
-    if isinstance(vertices, np.ndarray):
-        vertices = Vector3dVector(vertices)
+    if isinstance(vertices, np.ndarray): vertices = Vector3dVector(vertices)
     pcd = PointCloud(vertices)
     center = pcd.get_center()[np.newaxis, :]
     distances_to_center = pcd.compute_point_cloud_distance(PointCloud(Vector3dVector(center)))
@@ -38,8 +36,27 @@ def compute_inner_sphere(vertices:Union[np.ndarray, Vector3dVector]) -> Tuple[Un
     return center, max_radius
 
 def project_to_sphere(vertices:Union[np.ndarray, Vector3dVector], center: np.ndarray, radius: float) -> np.ndarray:
-    if isinstance(vertices, Vector3dVector):
-        vertices = np.asarray(vertices)
+    if isinstance(vertices, Vector3dVector): vertices = np.asarray(vertices)
     m = vertices - center
     m /= np.linalg.norm(m, axis=1, keepdims=True)
     return center + radius * m
+
+def cartesian_to_spherical_coords(vertices:Union[np.ndarray, Vector3dVector]) -> np.ndarray:
+    if isinstance(vertices, Vector3dVector): vertices = np.asarray(vertices)
+    x, y, z = vertices[:,0], vertices[:,1], vertices[:,2]
+    spherical = np.empty_like(vertices)
+    ρ, φ, θ = spherical[:,0], spherical[:,1], spherical[:,2]
+    ρ[:] = np.linalg.norm(vertices, axis=1) 
+    φ[:] = np.arccos(z/ρ)
+    θ[:] = np.arctan2(y, x)
+    return spherical
+
+def spherical_to_cartesian_coords(vertices:Union[np.ndarray, Vector3dVector]) -> np.ndarray:
+    if isinstance(vertices, Vector3dVector): vertices = np.asarray(vertices)
+    ρ, φ, θ = vertices[:,0], vertices[:,1], vertices[:,2]
+    cartesian = np.empty_like(vertices)
+    x, y, z = cartesian[:,0], cartesian[:,1], cartesian[:,2]
+    x[:] = ρ*np.sin(φ)*np.cos(θ)
+    y[:] = ρ*np.sin(φ)*np.sin(θ)
+    z[:] = ρ*np.cos(φ)
+    return cartesian
